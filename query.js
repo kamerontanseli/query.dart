3c77cfe9f084d908773c78e34b6765c70b305d60
Kameron Tanseli <kameron@localhost.localdomain>
2016-10-08 23:37:02 +0100

    initial

---
+++ query.js
@@ -0,0 +1,35 @@
+var Qy = (function(){
+
+	var query_parser = function(query, value) {
+	    var query_comparisons = {
+	        "lt": function(b, a) { return a < b; }, // less than
+	        "gt": function(b, a) { return a > b; }, // greater than
+	        "eq": function(b, a) { return a === b; },
+	        "not": function(b, a) { return b !== a; },
+	        "contain": function(b, a) { return a.indexOf(b) > -1; },
+	        "rgx": function(b, a) { return a.match(b); } // if regex match any
+	    };
+
+	    var queries = query.split("__"), // id__gt ---> id, gt
+		    partial_funcs = query_comparisons[queries[1]].bind(void 0, value);
+		    // set the first param to value and return function
+
+
+	    return function(obj) {
+	    	return partial_funcs(obj[queries[0]]); // id__gt --> gt(obj[id])
+	    };
+	};
+	
+	var query_chainer = function(query) {
+		var comparison = Object.keys(query) // id__lt: 2
+			.map(function(k){
+				return query_parser(k, query[k]); // lt(id)(obj)
+			});
+	    return function(obj){
+	        return comparison.every(function(f){ return f(obj); });
+	    };
+	};
+
+	return query_chainer;
+})();
+
