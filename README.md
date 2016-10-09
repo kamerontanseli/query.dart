24a98bae872557a7864e740142c7d9fabac9ce3a
Kameron Tanseli <kameron@localhost.localdomain>
2016-10-09 16:17:36 +0100

    version 2.0.0

--- README.md
+++ README.md
@@ -20,8 +20,39 @@ Query.js
 | gte    | a >= b     |  id__gte: 5 |
 | eq     | a === b    |  id__eq: 2 |
 | not    | a !== b    |  title__not: "Hello" |
-| contain | a.indexOf(b) > -1 |  title__contains: "hello" |
+| contain | a.indexOf(b) > -1 |  title__contain: "hello" |
 | icontain | a.toLowerCase().indexOf(b) > -1 |  title__icontain: /[hello]/g |
 | rgx    | a.match(b) |  title__rgx: /[hello]/g |
 | irgx    | a.toLowerCase().match(b) |  title__irgx: /[hello]/g |
 | in | b.indexOf(a) > -1 |  id__in: [1,2,3] |
+
+### Supports Nested Objects and methods
+
+```javascript
+var sample_data = [
+    {
+     id: 2,
+     time: new Date(),
+     user: {
+        username: "kameron"
+     }
+    },
+    {
+     id: 3,
+     time: new Date(),
+     user: {
+        username: "bradley"
+     }
+    },
+];
+
+
+var get_user = Qy({
+    "time__getFullYear__eq": 2016, // method
+    "user__username__icontain": "ron"// object
+});
+
+sample_data.filter(get_users_from_2016);
+// [id: 2...]
+
+```
--- query.js
+++ query.js
@@ -1,77 +1,85 @@
-(function(root, factory) {
-    if (typeof define === "function" && define.amd) {
-        define(["postal"], function(postal) {
-            return (root.Qy = factory(postal));
-        });
-    } else if (typeof module === "object" && module.exports) {
-        module.exports = (root.Qy = factory(require("postal")));
-    } else {
-        root.Qy = factory(root.postal);
-    }
-}(this, function(postal) {
+"use strict";
 
-    var query_parser = function(query, value) {
-        var query_comparisons = {
-            "lt": function(b, a) {
-                return a < b;
-            }, // less than
-            "lte": function(b, a) {
-                return a <= b;
-            },
-            "gt": function(b, a) {
-                return a > b;
-            }, // greater than
-            "gte": function(b, a) {
-                return a >= b;
-            },
-            "eq": function(b, a) {
-                return a === b;
-            },
-            "not": function(b, a) {
-                return b !== a;
-            },
-            "iexact": function(b, a) {
-                return b.toLowerCase() === a.toLowerCase();
-            },
-            "icontain": function(b, a) {
-                return a.toLowerCase().indexOf(b.toLowerCase()) > -1;
-            },
-            "in": function(b, a) {
-                return this.contain(a, b);
-            },
-            "contain": function(b, a) {
-                return a.indexOf(b) > -1;
-            },
-            "irgx": function(b, a) {
-                return a.toLowerCase().match(b);
-            },
-            "rgx": function(b, a) {
-                    return a.match(b);
-                } // if regex match any
-        };
-
-        var queries = query.split("__"), // id__gt ---> id, gt
-            partial_funcs = query_comparisons[queries[1]].bind(void 0, value);
-        // set the first param to value and return function
-
-
-        return function(obj) {
-            return partial_funcs(obj[queries[0]]); // id__gt --> gt(obj[id])
-        };
+window.Qy = (function() {
+    var comparisons = {
+        "lt": function(b, a) {
+            return a < b;
+        }, // less than
+        "lte": function(b, a) {
+            return a <= b;
+        },
+        "gt": function(b, a) {
+            return a > b;
+        }, // greater than
+        "gte": function(b, a) {
+            return a >= b;
+        },
+        "eq": function(b, a) {
+            return a === b;
+        },
+        "not": function(b, a) {
+            return b !== a;
+        },
+        "iexact": function(b, a) {
+            return b.toLowerCase() === a.toLowerCase();
+        },
+        "icontain": function(b, a) {
+            return a.toLowerCase().indexOf(b.toLowerCase()) > -1;
+        },
+        "contain": function(b, a) {
+            return a.indexOf(b) > -1;
+        },
+        "in": function(b, a) {
+            return b.indexOf(a) > -1;
+        },
+        "irgx": function(b, a) {
+            return a.toLowerCase().match(b);
+        },
+        "rgx": function(b, a) {
+                return a.match(b);
+        } // if regex match any
     };
 
-    var query_chainer = function(query) {
-        var comparison = Object.keys(query) // id__lt: 2
-            .map(function(k) {
-                return query_parser(k, query[k]); // lt(id)(obj)
-            });
+    function depth_property(levels, obj) {
+        return levels.reduce(function(a, b) {
+            return typeof a[b] === "function" ? a[b]() : a[b];
+        }, obj);
+    }
+
+    function parse(query, value) {
+        var queries = query.split("__"),
+            props = queries.filter(function(a) {
+                return !(a in comparisons)
+            }),
+            compare = queries
+	            .filter(function(a) {
+	                return a in comparisons;
+	            })
+	            .map(function(a) {
+	                return comparisons[a].bind(void 0, value);
+	            });
+
         return function(obj) {
-            return comparison.every(function(f) {
+            var obj_prop = depth_property(props, obj);
+            if(typeof obj_prop === "function") 
+            	obj_prop = obj_prop.bind(obj)();
+
+            return compare.every(function(f) {
+                return f(obj_prop);
+            });
+        }
+    }
+
+    return function(query_obj) {
+        var funcs = Object.keys(query_obj).map(function(k) {
+            return parse(k, query_obj[k]);
+        });
+
+        return function(obj) {
+            return funcs.every(function(f) {
                 return f(obj);
             });
         };
     };
 
-    return query_chainer;
-
-}));
+})();
